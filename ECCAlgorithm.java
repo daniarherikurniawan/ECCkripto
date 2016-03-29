0dbe04d6af5a47f46d6d25f5102a17ab31111263
Muhamad Fakhrusy <v8areu@gmail.com>
2016-03-29 09:04:16 +0000

    udah bisa iterasi penjumlahan titik sesuai halaman 52 slide gan

--- ECCAlgorithm.java
+++ ECCAlgorithm.java
@@ -46,6 +46,23 @@ public class ECCAlgorithm {
             return result;
         }
 
+        public static int lambdaAddition(int xp, int yp, int xq, int yq) {
+            BigInteger primeNumber = new BigInteger("11");//prime number
+            int a = 1; //from function E.
+            int nominator = Math.abs(yp - yq);
+            int denominator = Math.abs(xp - xq);
+
+            BigInteger bigNominator = BigInteger.valueOf((long)nominator);
+            BigInteger bigDenominator = BigInteger.valueOf((long)denominator);
+            BigInteger inverseDenominator = bigDenominator.modInverse(primeNumber);            
+
+            int intInverseDenominator = inverseDenominator.intValue();
+            int intPrimeNumber = primeNumber.intValue();            
+            int result = (nominator*intInverseDenominator)%intPrimeNumber;
+            //System.out.println("nominator: " + bigNominator + "\ndenominator: " + bigDenominator + "\nInverse denominator: " + intInverseDenominator + "\nResult: " + result);
+            return result;
+
+        }
 	public static void main(String[] args) {
             int primeNumber = 11;
             int plainText = 2; //arbitrary plaintext
@@ -53,23 +70,36 @@ public class ECCAlgorithm {
             int nilaiY;
             int lambdaDup;
             int lambdaAdd;
-            int privateKey = 2;
-            int xr, yr;
+            int tempLambda;
+            int privateKey = 5; 
+            int xr, yr, tempX, tempY;
+            int i; //for iterating
             Point basis = new Point(0,1); //basis as public key
             //System.out.println("\n" + koordinat.getY() + "\n");
             //koordinat = new Point(koordinat.getX(),nilaiBaruY);
             nilaiY = functionECC(nilaiX);
             Point PM= new Point(nilaiX,nilaiY);
             System.out.println(PM.toString());
-            lambdaDup = lambdaDuplication(nilaiX, nilaiY);
             
             /*
              * hitung koordinat xr dan yr
             */
-            xr = ((int)Math.pow(lambdaDup, 2) - 2*nilaiX)%primeNumber;
-            yr = (((lambdaDup*(nilaiX-xr)) - nilaiY)%primeNumber + primeNumber)%primeNumber; // (a % b + b) % b modulo for giving positive value (a%b give negative!)
-            System.out.println("\nNilai lambda: " + lambdaDup);
-            System.out.println("\nnilai xr dan yr: " + xr + " " + yr);
+            xr = 0;
+            yr = 0;
+            tempX = nilaiX;
+            tempY = nilaiY;
+            lambdaDup = lambdaDuplication(nilaiX, nilaiY);
+            tempLambda = lambdaDup;
+            for (i=1; i<privateKey; i++) {                
+            xr = (((int)Math.pow(tempLambda, 2) - nilaiX - tempX)%primeNumber + primeNumber)%primeNumber;
+            yr = (((tempLambda*(tempX-xr)) - tempY)%primeNumber + primeNumber)%primeNumber; // (a % b + b) % b modulo for giving positive value (a%b give negative!)
+            tempX = xr;
+            tempY = yr;
+            //System.out.println("Nilai lambda: " + tempLambda);
+            System.out.println("\nk = " + i + "\nnilai xr dan yr: " + xr + " " + yr + "\n---------------\n");
+            lambdaAdd = lambdaAddition(nilaiX, nilaiY, tempX, tempY);
+            tempLambda = lambdaAdd;
+            }
             //System.out.println("\n" + koordinat.getY());
             
 	}
