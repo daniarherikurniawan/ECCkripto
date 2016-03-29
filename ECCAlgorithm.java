bbbceef07a3c1f8ae1010276d2327d3721ade4cc
Muhamad Fakhrusy <v8areu@gmail.com>
2016-03-29 09:36:03 +0000

    udah bisa iterasi penjumlahan titik sesuai halaman 52 slide gan, maaf kodenya agak berantakan hahaha

--- ECCAlgorithm.java
+++ ECCAlgorithm.java
@@ -5,16 +5,18 @@ import java.math.BigInteger;
 
 public class ECCAlgorithm {
     
-    
+        static BigInteger primeNumber = new BigInteger("11");//prime number
+        static int intPrimeNumber = primeNumber.intValue(); //integer version of prime number            
+        static int a = 1; //coefficient of prime number
+
         public static int functionECC(int x) {
             //int result = 1;
             int ySquare; 
             int y = 1; // initialization of y
-            int primeNumber = 11; //prime number
-            ySquare = ((int)Math.pow(x, 3) + x + 6)%primeNumber ; // this is the E function y^2 = x^3 + ax + b
+            ySquare = ((int)Math.pow(x, 3) + x + 6)%intPrimeNumber ; // this is the E function y^2 = x^3 + ax + b
             //System.out.println("nilai ySquare: " + ySquare + "\n");
             
-            while ( ((int)Math.pow(y,2)-ySquare)%primeNumber != 0 )  {
+            while ( ((int)Math.pow(y,2)-ySquare)%intPrimeNumber != 0 )  {
                 //System.out.println((int)Math.pow(y,2));                
                 y += 1;
                 if (y == 100000) {
@@ -30,8 +32,6 @@ public class ECCAlgorithm {
         
         public static int lambdaDuplication(int xp, int yp) {
             
-            BigInteger primeNumber = new BigInteger("11");//prime number
-            int a = 1; //from function E.
             int nominator = 3*(int)Math.pow(xp,2) + a;
             BigInteger bigNominator = BigInteger.valueOf((long)nominator);
             int denominator = 2*yp;
@@ -39,7 +39,6 @@ public class ECCAlgorithm {
             BigInteger inverseDenominator = bigDenominator.modInverse(primeNumber);
             
             int intInverseDenominator = inverseDenominator.intValue();
-            int intPrimeNumber = primeNumber.intValue();
             
             int result = (nominator*intInverseDenominator)%intPrimeNumber;
             //System.out.println("nominator: " + bigNominator + "\ndenominator: " + bigDenominator + "\nInverse denominator: " + intInverseDenominator + "\nResult: " + result);
@@ -47,8 +46,6 @@ public class ECCAlgorithm {
         }
 
         public static int lambdaAddition(int xp, int yp, int xq, int yq) {
-            BigInteger primeNumber = new BigInteger("11");//prime number
-            int a = 1; //from function E.
             int nominator = Math.abs(yp - yq);
             int denominator = Math.abs(xp - xq);
 
@@ -57,14 +54,12 @@ public class ECCAlgorithm {
             BigInteger inverseDenominator = bigDenominator.modInverse(primeNumber);            
 
             int intInverseDenominator = inverseDenominator.intValue();
-            int intPrimeNumber = primeNumber.intValue();            
             int result = (nominator*intInverseDenominator)%intPrimeNumber;
             //System.out.println("nominator: " + bigNominator + "\ndenominator: " + bigDenominator + "\nInverse denominator: " + intInverseDenominator + "\nResult: " + result);
             return result;
 
         }
 	public static void main(String[] args) {
-            int primeNumber = 11;
             int plainText = 2; //arbitrary plaintext
             int nilaiX = plainText;
             int nilaiY;
@@ -91,8 +86,8 @@ public class ECCAlgorithm {
             lambdaDup = lambdaDuplication(nilaiX, nilaiY);
             tempLambda = lambdaDup;
             for (k=1; k<privateKey; k++) {                
-            xr = (((int)Math.pow(tempLambda, 2) - nilaiX - tempX)%primeNumber + primeNumber)%primeNumber;
-            yr = (((tempLambda*(tempX-xr)) - tempY)%primeNumber + primeNumber)%primeNumber; // (a % b + b) % b modulo for giving positive value (a%b give negative!)
+            xr = (((int)Math.pow(tempLambda, 2) - nilaiX - tempX)%intPrimeNumber + intPrimeNumber)%intPrimeNumber;
+            yr = (((tempLambda*(tempX-xr)) - tempY)%intPrimeNumber + intPrimeNumber)%intPrimeNumber; // (a % b + b) % b modulo for giving positive value (a%b give negative!)
             tempX = xr;
             tempY = yr;
             //System.out.println("Nilai lambda: " + tempLambda);
