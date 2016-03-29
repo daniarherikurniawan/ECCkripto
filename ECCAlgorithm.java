763325fc18834a74616ba3bdb84660bb5981028b
Muhamad Fakhrusy <v8areu@gmail.com>
2016-03-29 07:55:05 +0000

    nambah dikit

--- ECCAlgorithm.java
+++ ECCAlgorithm.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.math.BigInteger;
 
 
 
@@ -7,29 +8,55 @@ public class ECCAlgorithm {
     
         public static int functionECC(int x) {
             //int result = 1;
-            int ySquare;
-            int y = 1;
-            int primeNumber = 11;
-            ySquare = ((int)Math.pow(x, 3) + x + 6)%primeNumber ;
-            System.out.println("nilai ySquare: " + ySquare + "\n");
+            int ySquare; 
+            int y = 1; // initialization of y
+            int primeNumber = 11; //prime number
+            ySquare = ((int)Math.pow(x, 3) + x + 6)%primeNumber ; // this is the E function y^2 = x^3 + ax + b
+            //System.out.println("nilai ySquare: " + ySquare + "\n");
             
             while ( ((int)Math.pow(y,2)-ySquare)%primeNumber != 0 )  {
-                System.out.println((int)Math.pow(y,2));                
+                //System.out.println((int)Math.pow(y,2));                
                 y += 1;
+                if (y == 100000) {
+                    System.out.println("Nilai y tidak ditemukan");
+                    y=0;
+                    break;
+                }
             }
             
             return y;
             
         } 
+        
+        public static int lambdaDuplication(int xp, int yp) {
+            
+            BigInteger primeNumber = new BigInteger("11");//prime number
+            int a = 1; //from function E.
+            int nominator = 3*(int)Math.pow(xp,2) + a;
+            BigInteger bigNominator = BigInteger.valueOf((long)nominator);
+            int denominator = 2*yp;
+            BigInteger bigDenominator = BigInteger.valueOf((long)denominator);
+            BigInteger inverseDenominator = bigDenominator.modInverse(primeNumber);
+            System.out.println("nominator: " + bigNominator + "\ndenominator: " + bigDenominator + "\nInverse denominator: " + inverseDenominator);
+            return 0;
+        }
 
 	public static void main(String[] args) {
-            Point koordinat= new Point(8,2);
-            int nilaiX = koordinat.getX();
-            int powerX;
+            int plainText = 2; //arbitrary plaintext
+            int nilaiX = plainText;
             int nilaiY;
+            int lambda;
+            int privateKey = 2;
+            Point basis = new Point(0,1); //basis as public key
+            //System.out.println("\n" + koordinat.getY() + "\n");
+            //koordinat = new Point(koordinat.getX(),nilaiBaruY);
             nilaiY = functionECC(nilaiX);
-            powerX = (int)Math.pow(nilaiX,3);
-            System.out.println("\n" + nilaiX + " dan " + nilaiY);
+            Point PM= new Point(nilaiX,nilaiY);
+
+            System.out.println(PM.toString());
+            lambda = lambdaDuplication(nilaiX, nilaiY);
+            
+            //System.out.println("\n" + koordinat.getY());
             
 	}
 
--- Point.java
+++ Point.java
@@ -9,8 +9,8 @@
  * @author 
  */
 public class Point {
-    private final int x;
-    private final int y;
+    private int x;
+    private int y;
 
     public Point(int x, int y) {
         this.x = x;
