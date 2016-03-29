c80ecdeb2ee420f2ec06c6927871005226a058c4
Muhamad Fakhrusy <v8areu@gmail.com>
2016-03-29 00:41:03 +0000

    nambah point.java

--- ECCAlgorithm.java
+++ ECCAlgorithm.java
@@ -2,8 +2,7 @@
 public class ECCAlgorithm {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world lalalalalalal sdsds");
-
+            System.out.println(new Point(5.0, 5.0).toString());
 	}
 
 }
